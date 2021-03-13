b7d6d0793c64bf0d412cbb3e70592f7561ca224b
Marcos Di Paolo <marcos.dipaolo@softvision.com>
2021-03-13 20:10:17 -0300

    extracted post data to another component

--- src/components/pages/Blog/SinglePost.js
+++ src/components/pages/Blog/SinglePost.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux'
 import {getPost} from "../../../actions";
 import {useEffect} from 'react'
-import thumbnailResolver from "./thumbnailResolver";
+import PostData from './PostData'
 import Spinner from "../../Spinner";
 
 const SinglePost = ({post, match, getPost, currentPage}) => {
@@ -11,43 +11,12 @@ const SinglePost = ({post, match, getPost, currentPage}) => {
             getPost(null)
         }
     }, [getPost, match.params.id])
-
-    const resolveTags = () => {
-        const termsArray = post._embedded['wp:term'] ? post._embedded['wp:term'][1] || null : null
-        if (!termsArray) {
-            return ''
-        }
-        return termsArray.map(t => t.name).join(', ')
-    }
-    // if (!post || !post._embedded) {
-    //     return <div className="position-relative" style={{minHeight: '200px'}}><Spinner/></div>
-    // }
     return (
         <section className="container px-25 single-post pt-25">
             <h1 className='big d-none d-sm-block mt-60'>my <span>blog</span></h1>
             <p className="title-bg d-none d-sm-block">posts</p>
-            {(post && post._embedded) ?
-                <div>
-                    <div className="meta">
-                        <small>
-                            <span className="author">
-                                <i className="fa fa-user"/>
-                                {post._embedded.author[0].name}
-                            </span>
-                            <span className="date">
-                                <i className="fa fa-calendar"/>
-                                {new Date(post.date).toLocaleDateString()}
-                            </span>
-                            <span className="tags">
-                                <i className="fa fa-tags"/>
-                                {resolveTags()}
-                            </span>
-                        </small>
-                    </div>
-                    <h2 className="mt-10 mb-25 fw-600" dangerouslySetInnerHTML={{__html: post.title.rendered}}/>
-                    <img className={"mb-20"} src={thumbnailResolver(post, 'full')} alt={'post_image'}/>
-                    <div className="content" dangerouslySetInnerHTML={{__html: post.content.rendered}}/>
-                </div> :
+            {(post && post._embedded) ? <PostData post={post}/>
+                 :
                 <div className="position-relative" style={{minHeight: '200px'}}>
                     <Spinner/>
                 </div>
