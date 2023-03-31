5ace41dc13d9269c18292396ea1d762a5f0f42e4
Marcos Di Paolo <mdipaolo@nerdwallet.com>
2023-03-31 16:13:20 -0300

    fixed title - deploy on merge works

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = (props) => {
               {t('pages.home.im')}
               <span>
                 {' '}
-                Marcos Di Paolo on Render
+                Marcos Di Paolo
               </span>
             </h1>
             <p className=" lh-32">
