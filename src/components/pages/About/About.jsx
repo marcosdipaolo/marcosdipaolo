f7b160a853748d94604e81e3afceaabb61f1e12f
Marcos Di Paolo <mdipaolo@nerdwallet.com>
2023-04-09 12:44:22 -0300

    removed mock image from about

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -12,9 +12,9 @@ const About = () => (
       <div className="row">
         <div className="col-12 col-lg-5 col-xl-7">
           <h3 className="col-12 text-upper my-23"><Trans i18nKey="pages.about.personalInfos" /></h3>
-          <div className="image-container col-12 d-block d-sm-none px-10 mb-25">
-            <img src="/images/portrait-mobile.jpg" alt="" />
-          </div>
+          {/* <div className="image-container col-12 d-block d-sm-none px-10 mb-25"> */}
+          {/*  <img src="/images/portrait-mobile.jpg" alt="" /> */}
+          {/* </div> */}
           <ProfileData />
         </div>
         <Achievements />
